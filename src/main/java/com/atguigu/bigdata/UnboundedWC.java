d95e554d93f5cfeb13d3a22247a7802eba51af7e
liqibo <1289758842@qq.com>
2022-04-22 13:49:57 +0800

    Initial commit

---
+++ src/main/java/com/atguigu/bigdata/UnboundedWC.java
@@ -0,0 +1,40 @@
+package com.atguigu.bigdata;
+
+import org.apache.flink.api.common.functions.FlatMapFunction;
+import org.apache.flink.api.common.functions.MapFunction;
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.streaming.api.datastream.DataStreamSource;
+import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.util.Collector;
+
+
+/**
+ * @Author: liqibo
+ * @Date: 2022/04/22/13:39
+ * @Description:
+ */
+public class UnboundedWC {
+    public static void main(String[] args) {
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        env.setParallelism(1);
+
+        DataStreamSource<String> line = env.socketTextStream("hadoop102", 9999);
+
+        SingleOutputStreamOperator<String> WorldStream = line.flatMap(new FlatMapFunction<String, String>() {
+            @Override
+            public void flatMap(String value, Collector<String> out) throws Exception {
+                for (String world : value.split(" ")) {
+                    out.collect(world);
+                }
+            }
+        });
+
+        WorldStream.map(new MapFunction<String, Tuple2<String,Long>>() {
+            @Override
+            public Tuple2<String, Long> map(String value) throws Exception {
+                return Tuple2.of(value,1L);
+            }
+        })
+    }
+}
