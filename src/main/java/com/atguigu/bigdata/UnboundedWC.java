f83ec08c6e02b9254d0b27e52656aa81df0ec8b5
liqibo <1289758842@qq.com>
2022-04-22 14:12:20 +0800

    1

--- src/main/java/com/atguigu/bigdata/UnboundedWC.java
+++ src/main/java/com/atguigu/bigdata/UnboundedWC.java
@@ -2,6 +2,7 @@ package com.atguigu.bigdata;
 
 import org.apache.flink.api.common.functions.FlatMapFunction;
 import org.apache.flink.api.common.functions.MapFunction;
+import org.apache.flink.api.java.functions.KeySelector;
 import org.apache.flink.api.java.tuple.Tuple2;
 import org.apache.flink.streaming.api.datastream.DataStreamSource;
 import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
@@ -15,7 +16,7 @@ import org.apache.flink.util.Collector;
  * @Description:
  */
 public class UnboundedWC {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
         env.setParallelism(1);
 
@@ -30,11 +31,19 @@ public class UnboundedWC {
             }
         });
 
-        WorldStream.map(new MapFunction<String, Tuple2<String,Long>>() {
+        SingleOutputStreamOperator<Tuple2<String, Long>> MapStream = WorldStream.map(new MapFunction<String, Tuple2<String, Long>>() {
             @Override
             public Tuple2<String, Long> map(String value) throws Exception {
-                return Tuple2.of(value,1L);
+                return Tuple2.of(value, 1L);
             }
-        })
+        });
+
+        MapStream.keyBy(new KeySelector<Tuple2<String, Long>, String>() {
+            @Override
+            public String getKey(Tuple2<String, Long> value) throws Exception {
+                return value.f0;
+            }
+        }).sum(1).print();
+        env.execute();
     }
 }
